7c805d7386f598afd37b8eed2e83af6f6512d281
Reza_Atom <info@rp76.ir>
2021-02-03 22:55:06 +0330

    add main route

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return redirect(route("root"));
+});
+
 Route::group(['prefix' => 'dashboard',"middleware"=>"auth","namespace"=>"Dashboard"], function () {
     Route::get('/',[DashboardController::class,"Index"])->name("root");
 });
